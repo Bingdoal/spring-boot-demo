9de565a7dc4ff861b9b4b1e45c2422cbd392b325
bingdoal <jjohhnbooboo@gmail.com>
2021-09-29 14:08:12 +0800

    fix: set StatusException message in JsonNode.

--- src/main/java/springboot/test/exception/StatusException.java
+++ src/main/java/springboot/test/exception/StatusException.java
@@ -9,12 +9,10 @@ import lombok.EqualsAndHashCode;
 @Data
 public class StatusException extends Exception {
     private int code;
-    private String message;
     private JsonNode jsonNode;
 
     public StatusException(int httpStatus, String message) {
         super(message);
-        this.message = message;
         this.code = httpStatus;
         this.jsonNode = new ObjectMapper().createObjectNode().put("message", message);
     }
