1e7af2a703163d43274de34783407d3a5f722ef7
bingdoal <jjohhnbooboo@gmail.com>
2021-10-27 16:38:15 +0800

    feat: add manual validation demo.

--- src/main/java/springboot/test/controller/DemoController.java
+++ src/main/java/springboot/test/controller/DemoController.java
@@ -2,14 +2,18 @@ package springboot.test.controller;
 
 import io.swagger.annotations.Api;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import springboot.test.dto.UserDto;
 import springboot.test.dto.basic.ValidList;
 
+import javax.validation.ConstraintViolation;
 import javax.validation.Valid;
+import javax.validation.Validator;
 import java.util.List;
+import java.util.Set;
 
 @Api(tags = "Test")
 @Slf4j
@@ -17,6 +21,8 @@ import java.util.List;
 @RequestMapping("/v1/test")
 @Validated
 public class DemoController {
+    @Autowired
+    Validator validator;
 
     @PostMapping("/listValidation")
     @ResponseStatus(HttpStatus.OK)
@@ -29,5 +35,16 @@ public class DemoController {
     @ResponseStatus(HttpStatus.OK)
     public void testValidList(@RequestBody @Validated(UserDto.Create.class) ValidList<UserDto> userDtoList) {
 
+    }
+
+    @PostMapping("/validManual")
+    @ResponseStatus(HttpStatus.OK)
+    public void validManual(@RequestBody UserDto userDto) {
+        Set<ConstraintViolation<UserDto>> validateSet = validator.validate(userDto, UserDto.class);
+        if (!validateSet.isEmpty()) {
+            for (ConstraintViolation<UserDto> violation : validateSet) {
+                System.out.println(violation.getMessage());
+            }
+        }
     }
 }
