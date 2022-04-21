d05a5a025ee9e1e39c328badf09f4a9348e15748
bingdoal <jjohhnbooboo@gmail.com>
2022-04-21 09:48:49 +0800

    feat: add lock at dump db.

--- src/main/java/springboot/demo/service/DbDumpService.java
+++ src/main/java/springboot/demo/service/DbDumpService.java
@@ -8,6 +8,9 @@ import springboot.demo.middleware.exception.StatusException;
 import springboot.demo.utils.properties.DatasourceInfo;
 import springboot.demo.utils.properties.InfluxInfo;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 @Service
 @Slf4j
 public class DbDumpService {
@@ -18,33 +21,39 @@ public class DbDumpService {
     @Autowired
     private InfluxInfo influxInfo;
 
-    private boolean postgresLock = false;
-    private boolean influxdbLock = false;
+    private final Lock postgresLock = new ReentrantLock();
+    private final Lock influxdbLock = new ReentrantLock();
 
     public void dumpPostgres(String outputFile) throws StatusException {
-        if (postgresLock) {
+        if (!postgresLock.tryLock()) {
             throw new StatusException(400, "PostgreSQL dump is running...");
         }
-        postgresLock = true;
-        String setEnv;
-        String cmd;
-        if (System.getProperty("os.name").toLowerCase().contains("windows")) {
-            setEnv = "set PGPASSWORD=" + datasourceInfo.getPassword() + "&& ";
-            cmd = setEnv + " pg_dump -h " + datasourceInfo.getHost() + " -p " + datasourceInfo.getPort() +
-                    " -U " + datasourceInfo.getUsername() + " -d " + datasourceInfo.getDatabase() +
-                    " -f " + outputFile;
-        } else {
-            setEnv = "export PGPASSWORD=" + datasourceInfo.getPassword() + "&& ";
-            cmd = setEnv + " /usr/bin/pg_dump -h " + datasourceInfo.getHost() + " -p " + datasourceInfo.getPort() +
-                    " -U " + datasourceInfo.getUsername() + " -d " + datasourceInfo.getDatabase() +
-                    " -f " + outputFile;
-        }
-        CmdResultDto resultDto = cmdExecuteService.execute(cmd);
-        postgresLock = false;
-        if (!resultDto.isSuccess()) {
-            throw new StatusException(500, "Dump postgres failed: "
-                    + resultDto.getExitCode() + ": "
-                    + resultDto.getError());
+        try {
+            String setEnv;
+            String cmd;
+            if (System.getProperty("os.name").toLowerCase().contains("windows")) {
+                setEnv = "set PGPASSWORD=" + datasourceInfo.getPassword() + "&& ";
+                cmd = setEnv + " pg_dump -h " + datasourceInfo.getHost() + " -p " + datasourceInfo.getPort() +
+                        " -U " + datasourceInfo.getUsername() + " -d " + datasourceInfo.getDatabase() +
+                        " -f " + outputFile;
+            } else {
+                setEnv = "export PGPASSWORD=" + datasourceInfo.getPassword() + "&& ";
+                cmd = setEnv + " /usr/bin/pg_dump -h " + datasourceInfo.getHost() + " -p " + datasourceInfo.getPort() +
+                        " -U " + datasourceInfo.getUsername() + " -d " + datasourceInfo.getDatabase() +
+                        " -f " + outputFile;
+            }
+            CmdResultDto resultDto = cmdExecuteService.execute(cmd);
+            if (!resultDto.isSuccess()) {
+                throw new StatusException(500, "Dump postgres failed: "
+                        + resultDto.getExitCode() + ": "
+                        + resultDto.getError());
+            }
+        } catch (StatusException ex) {
+            throw ex;
+        } catch (Exception e) {
+            throw new StatusException(500, "Dump postgres failed: " + e.getMessage());
+        } finally {
+            postgresLock.unlock();
         }
     }
 
@@ -52,22 +61,28 @@ public class DbDumpService {
         if (System.getProperty("os.name").toLowerCase().contains("windows")) {
             throw new StatusException(500, "Influx dump cli not support on Windows");
         }
-        if (influxdbLock) {
+        if (!influxdbLock.tryLock()) {
             throw new StatusException(400, "InfluxDB dump is running...");
         }
-        influxdbLock = true;
-        String dumpDir = "influx_dump_" + System.currentTimeMillis();
-        String cmd = "/usr/bin/influxd backup -portable -db " + influxInfo.getDatabase() +
-                " -host " + influxInfo.getHost() + ":" + influxInfo.getBackupPort() +
-                " " + dumpDir;
-        cmd += " && tar -cf " + outputFile + " " + dumpDir;
-        cmd += " && rm -rf " + dumpDir;
-        CmdResultDto resultDto = cmdExecuteService.execute(cmd);
-        influxdbLock = false;
-        if (!resultDto.isSuccess()) {
-            throw new StatusException(500, "Dump influxdb failed: "
-                    + resultDto.getExitCode() + ": "
-                    + resultDto.getError());
+        try {
+            String dumpDir = "influx_dump_" + System.currentTimeMillis();
+            String cmd = "/usr/bin/influxd backup -portable -db " + influxInfo.getDatabase() +
+                    " -host " + influxInfo.getHost() + ":" + influxInfo.getBackupPort() +
+                    " " + dumpDir;
+            cmd += " && tar -cf " + outputFile + " " + dumpDir;
+            cmd += " && rm -rf " + dumpDir;
+            CmdResultDto resultDto = cmdExecuteService.execute(cmd);
+            if (!resultDto.isSuccess()) {
+                throw new StatusException(500, "Dump influxdb failed: "
+                        + resultDto.getExitCode() + ": "
+                        + resultDto.getError());
+            }
+        } catch (StatusException ex) {
+            throw ex;
+        } catch (Exception e) {
+            throw new StatusException(500, "Dump influxdb failed: " + e.getMessage());
+        } finally {
+            influxdbLock.unlock();
         }
     }
 }
